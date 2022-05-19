787d63d44a494b0a7233990e50702d538bd2ca8d
rizkybiz <justin.defrank@hashicorp.com>
2022-05-19 16:57:32 -0400

    Working Azure Active Directory OIDC TF

--- azure/main.tf
+++ azure/main.tf
@@ -1,10 +1,15 @@
 terraform {
   required_providers {
     boundary = {
-      source = "hashicorp/boundary"
+      source  = "hashicorp/boundary"
       version = "1.0.7"
     }
   }
 }
 
-provider "boundary" {}
+provider "boundary" {
+  addr                            = var.boundary_addr
+  auth_method_id                  = var.auth_method_id
+  password_auth_method_login_name = var.username
+  password_auth_method_password   = var.password
+}
