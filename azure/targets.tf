a2cbedc9f6c22ed0ca50a2ec019e7460023b7038
rizkybiz <justin.defrank@hashicorp.com>
2022-05-26 16:42:37 -0400

    Updated target session count to -1 from 1 because boundary connect was failing at 1

--- azure/targets.tf
+++ azure/targets.tf
@@ -1,9 +1,10 @@
 resource "boundary_target" "demo_target" {
-  name         = "Demo Database Target"
-  description  = "A Demo Database Target"
-  type         = "tcp"
-  default_port = "5432"
-  scope_id     = boundary_scope.project.id
+  name                     = "Demo Database Target"
+  description              = "A Demo Database Target"
+  type                     = "tcp"
+  default_port             = "5432"
+  session_connection_limit = -1
+  scope_id                 = boundary_scope.project.id
   host_source_ids = [
     boundary_host_set_static.demo_host_set.id
   ]
