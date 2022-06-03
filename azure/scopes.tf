c353731494453b4d9f3bb7420a755e80e8dc85dc
rizkybiz <justin.defrank@hashicorp.com>
2022-06-03 13:20:11 -0400

    updated roles and scopes so the analysts and operations groups are tightly scoped to their own projects

--- azure/scopes.tf
+++ azure/scopes.tf
@@ -8,7 +8,6 @@ resource "boundary_scope" "org" {
   description              = "Scope for the demo organization"
   scope_id                 = boundary_scope.global.id
   auto_create_admin_role   = true
-  auto_create_default_role = true
 }
 
 resource "boundary_scope" "analysts_scope" {
