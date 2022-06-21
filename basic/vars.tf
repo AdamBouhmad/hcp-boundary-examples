2d3b3ba12a2f080703849796cebe7ea9a0c99b9e
PPacent <ppacent@hashicorp.com>
2022-06-21 22:53:33 +0200

    Updating for public consumption
    Updating for public consumption

--- basic/vars.tf
+++ basic/vars.tf
@@ -1,6 +1,6 @@
 variable "origin_url" {
     type = string
-    # Add your origin URL here, 
+    description = "Boundary environment's origin URL"
     # You can find your origin once you've enabled Boundary at https://portal.cloud.hashicorp.com/
     # Note: cannot have a terminating '/' character
     default = ""
@@ -8,21 +8,24 @@ variable "origin_url" {
 
 variable "auth_method_id" {
     type = string
-    # Login to your Boundary environment and copy the "password" auth method ID that was created at deployment
-    # Login -> Auth methods -> "password"
+    description = "Boundary auth method to authenticate TF"
+    # For new Boundary environments, login to your Boundary environment and copy the "password" auth method ID that was created at deployment
+    # IN HCP Portal Login -> Auth methods -> "password"
     default = "ampw_1234567890"
 }
 
 variable "bootstrap_user_login_name" {
     type = string
+    description = "Service account username for TF"
     # the username chosen of the user created when you enabled Boundary
-    default = "admin"
+    sensitive = true
 }
 
 variable "bootstrap_user_password" {
     type = string
+    description = "Service account password for TF"
     # the password chosen of the user created when you enabled Boundary
-    default = "password"
+    sensitive = true
 }
 
 variable "target_ips" {
